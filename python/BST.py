fabc943bc180a4d2d618e02b237e9a5222666e76
pettta <53843752+pettta@users.noreply.github.com>
2021-06-27 14:52:55 -0400

    implemented basic BST in python

---
+++ python/BST.py
@@ -0,0 +1,41 @@
+class Node():
+    def __init__(self, data):
+        self.data = data
+        self.right = None
+        self.left = None
+
+class BST():
+    def __init__(self):
+        self.root = None
+
+    def append(self, data):
+        newNode = Node(data)
+        if self.root is None:
+            self.root = newNode
+        else:
+            current = self.root
+            while True:
+                if data <= current.data:
+                    if current.left is None:
+                        current.left = newNode
+                        return
+                    else:
+                        current = current.left
+                else:
+                    if current.right is None:
+                        current.right = newNode
+                        return
+                    else:
+                        current = current.right 
+
+    def getMin(self):
+        current = self.root
+        while current.left is not None:
+            current = current.left
+        return current.data
+
+    def getMax(self):
+        current = self.root 
+        while current.right is not None:
+            current = current.right
+        return current.data 
