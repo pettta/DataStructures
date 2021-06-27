50b2a5f94898d5c681256786f92e096d5cdba18e
Tommy Pett <53843752+pettta@users.noreply.github.com>
2021-06-27 16:02:34 -0400

    Update BST.py

--- python/BST.py
+++ python/BST.py
@@ -8,7 +8,7 @@ class BST:
     def __init__(self):
         self.root = None
 
-    def append(self, data):
+    def insert(self, data):
         newNode = Node(data)
         if self.root is None:
             self.root = newNode
