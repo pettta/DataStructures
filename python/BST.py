e28ee9ee1bef620007c15006f86240bc5141e8e9
Tommy Pett <53843752+pettta@users.noreply.github.com>
2021-06-27 15:48:25 -0400

    Update BST.py

--- python/BST.py
+++ python/BST.py
@@ -1,10 +1,10 @@
-class Node():
+class Node:
     def __init__(self, data):
         self.data = data
         self.right = None
         self.left = None
 
-class BST():
+class BST:
     def __init__(self):
         self.root = None
 
